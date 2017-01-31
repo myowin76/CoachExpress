4c16ab202c7e8a75a2c0240c8124d6cdc158bfbc
Aung Myo Win <aungmyowin@akauk.com>
2017-01-31 17:58:50 +0000

    dashboard updates

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFire } from 'angularfire2';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+	user = {};
 
-  ngOnInit() {
+  constructor(public af: AngularFire, public router: Router) { 
+  	
+  	this.af.auth.subscribe(auth => {
+  		// console.log(auth.uid)	
+  		this.user = auth;
+
+  		if (auth){
+  			console.log(auth);	
+  			// get user profile
+  			// update menu link
+  		}else{
+  			// redirect to login page
+  		}
+  	});
   }
+
+	ngOnInit() {
+	}
+
+  	logout() {
+        this.af.auth.logout();
+        this.router.navigate(['/dashboard']);
+
+    }
+
+    toggleSignIn() {
+
+    }
 
 }
