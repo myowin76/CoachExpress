3efc954488fb13ea82a4ce845cd990f101b45521
Aung Myo Win <aungmyowin@akauk.com>
2017-02-01 12:14:11 +0000

    journey form scaffolded

---
+++ src/app/journey-form/journey-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-journey-form',
+  templateUrl: './journey-form.component.html',
+  styleUrls: ['./journey-form.component.scss']
+})
+export class JourneyFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
